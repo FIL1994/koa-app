c1d6bb153ca794984ed569ca5226c8382ca60813
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-04-02 19:32:53 -0400

    added jwt

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,11 @@
 import * as Router from "koa-router";
+import * as jwt from "jsonwebtoken";
 
 const router = new Router();
+router.get("/login", async ctx => {
+  ctx.body = jwt.sign({ data: "abc" }, "secret");
+});
+
 router.get("/*", async ctx => {
   ctx.body = "Hello World!";
 });
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import * as Koa from "koa";
 import * as helmet from "koa-helmet";
 import * as bodyParser from "koa-bodyparser";
+import * as jwt from "koa-jwt";
 import router from "./routes";
 
 const app = new Koa();
@@ -8,13 +9,15 @@ const app = new Koa();
 app.use(helmet());
 app.use(bodyParser());
 
+app.use(jwt({ secret: "secret" }).unless({ path: [/^\/login/] }));
+
 app.use(async (ctx, next) => {
   console.log("url", ctx.url);
 
   await next();
 });
 
-app.use(router.routes());
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
 
