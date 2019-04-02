8f7e48e0db288c6989f6165daefd86bcb572e0c6
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-04-02 19:14:52 -0400

    added helmet and body parser

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import * as Koa from "koa";
+import * as helmet from "koa-helmet";
+import * as bodyParser from "koa-bodyparser";
 import router from "./routes";
 
 const app = new Koa();
+
+app.use(helmet());
+app.use(bodyParser());
 
 app.use(async (ctx, next) => {
   console.log("url", ctx.url);
