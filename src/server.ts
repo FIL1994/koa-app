8625821ed88ca15853211898d86e6437273265a6
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-04-03 18:40:47 -0400

    setup typeorm (#1)
    * setup typeorm
    
    * added database to gitignore

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,31 @@
+import "reflect-metadata";
 import * as Koa from "koa";
 import * as helmet from "koa-helmet";
 import * as bodyParser from "koa-bodyparser";
 import * as jwt from "koa-jwt";
+import { createConnection } from "typeorm";
+import { User } from "./entity/User";
 import router from "./routes";
 
-const app = new Koa();
+createConnection()
+  .then(async connection => {
+    const app = new Koa();
 
-app.use(helmet());
-app.use(bodyParser());
+    app.use(helmet());
+    app.use(bodyParser());
 
-app.use(jwt({ secret: "secret" }).unless({ path: [/^\/login/] }));
+    app.use(jwt({ secret: "secret" }).unless({ path: [/^\/login/] }));
 
-app.use(async (ctx, next) => {
-  console.log("url", ctx.url);
+    app.use(async (ctx, next) => {
+      console.log("url", ctx.url);
 
-  await next();
-});
+      await next();
+    });
 
-app.use(router.routes()).use(router.allowedMethods());
+    app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(3000);
+    app.listen(3000);
 
-console.log("Server running on port 3000");
+    console.log("Server running on port 3000");
+  })
+  .catch(error => console.log(error));
