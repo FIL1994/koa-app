e72390de634a38c489a5cb4e909c6ca48d12e983
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-04-04 19:21:29 -0400

    added signup route

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,28 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+interface IUser {
+  firstName?: string;
+  lastName?: string;
+  age?: number;
+}
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column()
+  firstName: string;
 
-    @Column()
-    firstName: string;
+  @Column()
+  lastName: string;
 
-    @Column()
-    lastName: string;
+  @Column()
+  age: number;
 
-    @Column()
-    age: number;
-
+  constructor({ firstName, lastName, age }: IUser = {}) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,16 @@
+import { Context } from "koa";
 import * as Router from "koa-router";
 import * as jwt from "jsonwebtoken";
+import { User } from "./entity/User";
 
 const router = new Router();
-router.get("/login", async ctx => {
+router.get("/login", async (ctx: Context) => {
   ctx.body = jwt.sign({ data: "abc" }, "secret");
+});
+
+router.post("/signup", async (ctx: Context) => {
+  const user = new User(ctx.request.body);
+  ctx.body = user;
 });
 
 router.get("/*", async ctx => {
